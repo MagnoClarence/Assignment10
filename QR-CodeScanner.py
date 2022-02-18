676490ecde15727204daff5d0702e60ce76bac11
Magno <clarenceamagno@iskolarngbayan.pup.edu.ph>
2022-02-18 09:36:22 +0800

    First commit, the program is already working asking  whether to scan or generate QR code.

---
+++ QR-CodeScanner.py
@@ -0,0 +1,45 @@
+# QR Code Scanner
+import cv2 # For reading the QR code
+import webbrowser
+import pyqrcode # For creating the qr code
+import png # For exporting png files
+from pyqrcode import QRCode
+import datetime # For the date of when the QR code was read
+from datetime import date
+
+possibleInputs = ["Scan", "SCAN", "scan", "Make", "MAKE", "make"] 
+today = date.today()
+e = datetime.datetime.now()
+
+while 1:
+    start = str(input("Do you want to SCAN or MAKE QR code? (SCAN/MAKE):"))
+    if start in possibleInputs:
+        break
+    else:
+        print("enter SCAN or MAKE")
+if start in ["make","Make","MAKE"]:
+    content = input("Insert content for the QR code:")
+    url = pyqrcode.create(content)
+    fileName = str(input("What do you want to name the file?: "))
+    url.png(fileName + ".png",scale=6)
+else:
+    cap = cv2.VideoCapture(0)
+    detector = cv2.QRCodeDetector()
+    while True:
+        _,img = cap.read()  
+        data,one, _ = detector.detectAndDecode(img)
+        if data:
+            a = data
+            break
+        cv2.imshow("qrcodescanner app", img)
+        if cv2.waitKey(1)==ord("q"): # If you want to close the scanner, press "q"
+            break
+    b = webbrowser.open(str(a))
+
+    
+    text_file = open(str(today)+".txt", "w")
+    n = text_file.write("Data:" + "\n" + str(a) + "\n" + "Date scanned:" + "\n" + str(e))
+    text_file.close()
+    # cap.release(a)
+    cv2.destroyAllWindows
+    
